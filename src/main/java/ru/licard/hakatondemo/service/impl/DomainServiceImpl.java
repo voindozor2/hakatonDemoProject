3af64cc1fa6cb7dfb47a8e0719941cca8ac65650
notblank00 <pyromancy00@gmail.com>
2022-04-10 00:18:45 +0300

    сделал тупое решение, думаю дальше над нормальным

--- src/main/java/ru/licard/hakatondemo/service/impl/DomainServiceImpl.java
+++ src/main/java/ru/licard/hakatondemo/service/impl/DomainServiceImpl.java
@@ -1,7 +1,13 @@
 package ru.licard.hakatondemo.service.impl;
 
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
 import ru.licard.hakatondemo.data.HakatonEntityRepository;
 import ru.licard.hakatondemo.domain.HakatonEntity;
 import ru.licard.hakatondemo.dto.SendingDto;
@@ -9,14 +15,28 @@ import ru.licard.hakatondemo.service.DomainService;
 
 @Service
 @RequiredArgsConstructor
+@EnableTransactionManagement
 public class DomainServiceImpl implements DomainService {
 
     private final HakatonEntityRepository hakatonEntityRepository;
 
+    // этот обработчик проверяет что записи с таким ID в БД нет,
+    // после чего записывает новую запись
+
+    // т. к. Spring параллелит процессы самостоятельно,
+    // а проверка и запись производятся двумя разными вызовами,
+    // получается конкурентность, на момент проверки записи в БД нет
+    // а на момент попытки записать уже появилась
+
+    // я решил задачу по тупому, запретив параллельное выполнение
+    // этого метода словом synchronized,
+    // потому что у меня не работают транзакции
+    
     @Override
-    public void saveHakatonEntity(SendingDto sendingDto) {                                  // этот обработчик должен проверять, есть ли такая запись в БД,
-        if(!hakatonEntityRepository.existsHakatonEntityByName(sendingDto.getName())){       // и класть, только если ее нет
-            hakatonEntityRepository.save(new HakatonEntity(sendingDto.getName()));          // но почему-то он этого не делает
+    public synchronized void saveHakatonEntity(SendingDto sendingDto) {
+        if(!hakatonEntityRepository.existsHakatonEntityByName(sendingDto.getName())) {
+            System.out.println(sendingDto.getName() + hakatonEntityRepository.existsHakatonEntityByName(sendingDto.getName()));
+            hakatonEntityRepository.save(new HakatonEntity(sendingDto.getName()));
         }
     }
 }
