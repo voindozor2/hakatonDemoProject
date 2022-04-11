a4e9309a2ee6f4970df9fdbd1dd0d0161e445865
vadim.burns <vadim.burns@protonmail.com>
2022-04-11 14:45:43 +0300

    solution

--- src/main/java/ru/licard/hakatondemo/data/HakatonEntityRepository.java
+++ src/main/java/ru/licard/hakatondemo/data/HakatonEntityRepository.java
@@ -1,10 +1,18 @@
 package ru.licard.hakatondemo.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import ru.licard.hakatondemo.domain.HakatonEntity;
 
 import java.util.Optional;
 
 public interface HakatonEntityRepository extends JpaRepository<HakatonEntity, String> {
     Boolean existsHakatonEntityByName(String name);
+
+    @Query(value = "insert into hakaton_entity (name) values (?) ON CONFLICT DO NOTHING", nativeQuery = true)
+    @Transactional
+    @Modifying
+    void insertNewEntity(String name);
 }
