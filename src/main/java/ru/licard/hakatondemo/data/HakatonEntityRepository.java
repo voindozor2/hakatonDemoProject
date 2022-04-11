66dfe90742bc27dcfad6a8a0132c357138573144
vadim.burns <vadim.burns@protonmail.com>
2022-04-11 19:48:04 +0300

    fixed

--- src/main/java/ru/licard/hakatondemo/data/HakatonEntityRepository.java
+++ src/main/java/ru/licard/hakatondemo/data/HakatonEntityRepository.java
@@ -6,13 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 import ru.licard.hakatondemo.domain.HakatonEntity;
 
-import java.util.Optional;
-
 public interface HakatonEntityRepository extends JpaRepository<HakatonEntity, String> {
-    Boolean existsHakatonEntityByName(String name);
 
-    @Query(value = "insert into hakaton_entity (name) values (?) ON CONFLICT DO NOTHING", nativeQuery = true)
-    @Transactional
     @Modifying
-    void insertNewEntity(String name);
+    @Transactional
+    @Query(value = "insert into hakaton_entity (name) values (?) ON CONFLICT DO NOTHING", nativeQuery = true)
+    void createHakatonEntityIfNotExists(String name);
 }
--- src/main/java/ru/licard/hakatondemo/service/impl/DomainServiceImpl.java
+++ src/main/java/ru/licard/hakatondemo/service/impl/DomainServiceImpl.java
@@ -1,31 +1,19 @@
 package ru.licard.hakatondemo.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Isolation;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import ru.licard.hakatondemo.data.HakatonEntityRepository;
-import ru.licard.hakatondemo.domain.HakatonEntity;
 import ru.licard.hakatondemo.dto.SendingDto;
 import ru.licard.hakatondemo.service.DomainService;
-
-import javax.persistence.EntityManager;
 
 @Service
 @RequiredArgsConstructor
 public class DomainServiceImpl implements DomainService {
 
     private final HakatonEntityRepository hakatonEntityRepository;
-    private final EntityManager entityManager;
 
     @Override
     public void saveHakatonEntity(SendingDto sendingDto) {
-        if(!hakatonEntityRepository.existsHakatonEntityByName(sendingDto.getName())){
-//            hakatonEntityRepository.save(new HakatonEntity(sendingDto.getName()));
-            hakatonEntityRepository.insertNewEntity(sendingDto.getName());
-        }
+        hakatonEntityRepository.createHakatonEntityIfNotExists(sendingDto.getName());
     }
 }
